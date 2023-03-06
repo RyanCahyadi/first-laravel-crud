85f9a1b7c4d63f21b773bbbe108c18ff6848b199
RyanCahyadi <ryancahyadi11@gmail.com>
2023-03-07 02:10:46 +0700

    Add navigation

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', 'HomeController@home')->name('home');
+Route::get('/products', 'HomeController@viewProduct')->name('products');
+Route::get('/products/edit', 'HomeController@edit')->name('edit-product');
